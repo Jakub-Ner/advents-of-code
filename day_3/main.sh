78d50dfe04e74baf969c76fa5e325ad1ea7eb308
jakub-ner <kubaner1@gmail.com>
2024-12-09 21:32:21 +0100

    day 3

---
+++ day_3/main.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+FILE="input.txt"
+
+echo "Part 1"
+cat $FILE | \
+  egrep -o 'mul\([0-9]+,[0-9]+\)' | \
+  awk -F'[(,)]' '{sum += $2 * $3} END {print "Sum = "sum}'
+
+echo -e "\nPart 2"
+elements=$(cat $FILE | tr -d '\n' | \
+  egrep -o "mul\([0-9]+,[0-9]+\)|do()|don't\(\)" | \
+  awk -F'[(,)]'  '{print $1, $2*$3}')
+
+skip=false
+sum=0
+for element in $elements; do
+  if [ "$element" = "mul" ] || [ "$skip" = true ]; then
+    continue
+  fi
+
+  if [ "$element" = "do" ]; then
+    skip=false
+    continue
+  fi
+
+  if [ "$element" = "don't" ]; then
+    skip=true
+    continue
+  fi
+
+  sum=$((sum + element))
+done
+
+echo "Sum = $sum"
+
