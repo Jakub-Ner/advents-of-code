d85455778064ce8c3d4d958a7af55c2637374f4a
jakub-ner <kubaner1@gmail.com>
2024-12-13 01:36:06 +0100

    day 6 task 2

--- 2024/day_6/main.cpp
+++ 2024/day_6/main.cpp
@@ -1,5 +1,10 @@
+#include <cstring>
 #include <fstream>
+#include <functional>
 #include <iostream>
+#include <stdlib.h>
+#include <thread>
+#include <vector>
 
 using namespace std;
 
@@ -53,7 +58,16 @@ struct Pawn {
   enum Direction { N = 0, E, S, W } direction;
 };
 
+void print_visited(vector<Position> visitedPositions) {
+  cout << "Visited positions: ";
+  for (auto position : visitedPositions) {
+    cout << "(" << position.y << ", " << position.x << ") ";
+  }
+  cout << endl;
+}
+
 void draw_board(char **matrix, Pawn &pawn) {
+  system("clear");
   for (int i = 0; i < HEIGHT; i++) {
     for (int j = 0; j < WIDTH; j++) {
       if (i == pawn.position.y && j == pawn.position.x) {
@@ -109,19 +123,20 @@ bool is_out_of_bounds(Position &position) {
          position.x >= WIDTH;
 }
 
-enum Action { STOP, CONTINUE };
+enum Action { STOP, GO, ROTATE };
 
 Action move(Pawn &pawn, char **matrix) {
   Position *nextPosition = find_next_position(pawn);
-  Action action = CONTINUE;
+  Action action;
   if (is_out_of_bounds(*nextPosition)) {
     action = STOP;
   } else if (is_obstacle(matrix, *nextPosition)) {
     rotate(pawn);
+    action = ROTATE;
   } else {
     pawn.position = *nextPosition;
+    action = GO;
   }
-  // draw_board(matrix, pawn);
   delete nextPosition;
   return action;
 }
@@ -143,48 +158,100 @@ Pawn *find_pawn(char **matrix) {
       }
     }
   }
-  throw "Guard not found";
+  throw invalid_argument("Guard not found");
 }
+
 bool mark_guard_position(char **matrix, Pawn &pawn) {
   bool wasPositionUnvisited = false;
-  if (matrix[pawn.position.y][pawn.position.x] != VISITED_ICON) {
+  if (matrix[pawn.position.y][pawn.position.x] == UNVISITED_ICON) {
     matrix[pawn.position.y][pawn.position.x] = VISITED_ICON;
     wasPositionUnvisited = true;
   }
   return wasPositionUnvisited;
 }
 
-void run_task_1(char **matrix) {
-  cout << "Task 1" << endl;
+void add_obstruction(char **matrix, Position &position) {
+  matrix[position.y][position.x] = OBSTACLE_ICON;
+}
 
+void remove_obstruction(char **matrix, Position &position) {
+  matrix[position.y][position.x] = UNVISITED_ICON;
+}
+
+bool find_guard_positions(char **originalMatrix,
+                          function<void(const Pawn &)> onNewPositionCallback) {
+
+  char** matrix = new char*[HEIGHT];
+  for (int i = 0; i < HEIGHT; i++) {
+    matrix[i] = new char[WIDTH];
+    strcpy(matrix[i], originalMatrix[i]);
+  }
   Pawn *pawn = find_pawn(matrix);
+  Position startPosition = pawn->position;
   mark_guard_position(matrix, *pawn);
+  onNewPositionCallback(*pawn);
 
   int distincPlacesNum = 1;
-  const int INITIAL_COUNTDOWN = WIDTH * HEIGHT;
-  int countDown = INITIAL_COUNTDOWN;
+  int countDown = 1; 
   while (countDown > 0) {
     Action action = move(*pawn, matrix);
+    // std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    // draw_board(matrix, *pawn);
     if (action == STOP)
       break;
 
     if (mark_guard_position(matrix, *pawn)) {
       distincPlacesNum++;
-      countDown = INITIAL_COUNTDOWN;
-    } else {
+      countDown++;
+      onNewPositionCallback(*pawn);
+    } else if (action == GO) {
       countDown--;
     }
   }
-
+  clear_memory(matrix);
   delete pawn;
-  cout << "Number of distint positions guard visited: " << distincPlacesNum
-       << endl << endl;
+  return countDown > 0;
+}
+
+void run_task_2(char **matrix, vector<Position> visitedPositions) {
+  cout << "Task 2" << endl;
+
+  int possibleObstructions = 0;
+  for (int i = 1; i < visitedPositions.size(); i++) {
+    add_obstruction(matrix, visitedPositions[i]);
+    if (!find_guard_positions(matrix, [](const Pawn &pawn) {})) {
+      possibleObstructions++;
+    }
+    remove_obstruction(matrix, visitedPositions[i]);
+  }
+  cout << "Number of possible obstruction placements: " << possibleObstructions
+       << endl
+       << endl;
+}
+
+vector<Position> run_task_1(char **matrix) {
+  cout << "Task 1" << endl;
+
+  vector<Position> visitedPositions;
+  find_guard_positions(matrix, [&](const Pawn &pawn) {
+    visitedPositions.push_back(pawn.position);
+  });
+
+  cout << "Number of distint positions guard visited: "
+       << visitedPositions.size() << endl
+       << endl;
+
+  return visitedPositions;
 }
 
 int main() {
   char **matrix = new char *[HEIGHT];
   read_file(matrix, "./input.txt");
-  run_task_1(matrix);
+
+  auto visitedPositions = run_task_1(matrix);
+  // print_visited(visitedPositions);
+
+  run_task_2(matrix, visitedPositions);
   clear_memory(matrix);
   return 0;
 }
